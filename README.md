287f7b1aeb7d5eaaf05de269860fba535a248aa2
Nicholas Gabler <Nick@Macintosh.local>
2018-08-17 10:05:44 -0400

    inital commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # bitchute-deluge-rss-script
 A small bash script that will pull the rss feed for a bitchute channel, generate magnet links for all of the videos in the feed and then add them to deluge for seeding.
+
+The script should be configured as a cronjob to automatically pull and seed new videos.
---
+++ rss.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+## START CONFIG
+
+channel="channelname"
+deluge_host="127.0.0.1"
+deluge_port="58846"
+download_path="/your/download/path/"
+
+## END CONFIG
+
+deluge_console=$(which deluge-console)
+pull_guid=$(curl -s https://www.bitchute.com/feeds/rss/channel/$channel/ | grep -Eo "<guid>.{12}" | sed "s]<guid>]]g")
+
+for guid in $pull_guid; do
+	magnet=$(curl -s https://www.bitchute.com/video/$guid/ | grep -Eo "magnet.*\" data-toggle" | sed "s/\" data-toggle$//g")
+	$deluge_console --quiet "connect $deluge_host:$deluge_port; add -p $download_path $magnet" >> /dev/null
+done
